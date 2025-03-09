d916b9fd2f863ff5ccc30fe8fe02b06ca8e137da
Alain Plattner <alainplattner@gmail.com>
2025-03-09 14:06:02 -0500

    fixed normalization

--- specWiecTB.m
+++ specWiecTB.m
@@ -48,29 +48,42 @@ function intvals=intfun0(ls,cTH)
   intvals=nan(size(ls));
   x0=cos(cTH);
   for i=1:length(ls)
-    intvals(i) = 2/(2*ls(i)+1)*legendreprodint(ls(i),0,1,0,x0,'gl');
+    %intvals(i) = 2/(2*ls(i)+1)*legendreprodint(ls(i),0,1,0,x0,'gl');
+    intvals(i) = legendreprodint(ls(i),0,1,0,x0,'gl');
   end
 end
+
+% function intvals=intfun1(ls,cTH)  
+%   intvals=nan(size(ls));
+%   x0=cos(cTH);
+%   for i=1:length(ls)
+%     if ls(i)==1
+%       intvals(i) = 2/3 - x0 + x0^3/3;
+%     else
+%       intvals(i) = 4/(2*ls(i)-1)*legendreprodint(ls(i),1,1,1,x0,'gl');
+%     end
+%   end
+% end
 
 function intvals=intfun1(ls,cTH)  
   intvals=nan(size(ls));
   x0=cos(cTH);
   for i=1:length(ls)
-    if ls(i)==1
-      intvals(i) = 2/3 - x0 + x0^3/3;
-    else
-      intvals(i) = 4/(2*ls(i)-1)*legendreprodint(ls(i),1,1,1,x0,'gl');
-    end
+      intvals(i) = legendreprodint(ls(i),1,1,1,x0,'gl');
   end
 end
 
 function val=pval0(ls,cTH)
-  Pl0 = sqrt(2./(2*ls+1)).*xlm(ls,0,cTH);
+  % Pl0 = sqrt(2./(2*ls+1)).*xlm(ls,0,cTH);
+  % val = Pl0*sin(cTH)^2./(ls+2);
+  Pl0 = sqrt(4*pi./(2*ls+1)).*xlm(ls,0,cTH);
   val = Pl0*sin(cTH)^2./(ls+2);
 end
 
 function val=pval1(ls,cTH)
   % We have the condon-shortley phase. Remove it
-  Pl1 = - sqrt(4./(2*ls+1)).*xlm(ls,1,cTH);
+  %Pl1 = - sqrt(4./(2*ls+1)).*xlm(ls,1,cTH);
+  %val = Pl1*sin(cTH)*cos(cTH)./(ls+2);
+  Pl1 = -sqrt(8*pi./(2*ls+1)).*xlm(ls,1,cTH);
   val = Pl1*sin(cTH)*cos(cTH)./(ls+2);
 end
